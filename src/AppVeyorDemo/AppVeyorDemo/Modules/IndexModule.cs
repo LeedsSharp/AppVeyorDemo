29d37ccd751e1629b3a8c6bf76a9043beac59f9e
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-07-24 18:08:30 +0100

    Added base project

---
+++ src/AppVeyorDemo/AppVeyorDemo/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace AppVeyorDemo.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
