c937390d8429111176712bfe0eb0fada3aa33d42
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2014-07-29 15:27:09 +0100

    enable errors

--- src/AppVeyorDemo/AppVeyorDemo/AppVeyorBootstrapper.cs
+++ src/AppVeyorDemo/AppVeyorDemo/AppVeyorBootstrapper.cs
@@ -7,5 +7,11 @@ namespace AppVeyorDemo
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            StaticConfiguration.DisableErrorTraces = false;
+            base.ApplicationStartup(container, pipelines);
+        }
     }
 }
